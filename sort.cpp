3ed4e7c218b7d9c16c18e0a983cf7342db5d47fd
Devansh Khetan <56186839+Devanshk9@users.noreply.github.com>
2023-10-06 12:21:45 +0530

    Create sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+
+void selection_sort(int arr[], int n){
+
+for(int i = 0;i<=n-2;i++){
+    int mini = i;
+    for(int j = i; j<=n-1;j++){
+        if(arr[j] < arr[mini]){
+            mini=j;
+        }
+    }
+    int temp = arr[mini];
+    arr[mini] = arr[i];
+    arr[i] = temp;
+}
+
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    // typedef long long ll;
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) cin>>arr[i];
+    selection_sort(arr,n);
+    for (int i = 0; i < n; ++i)
+    {
+        cout<< arr[i]<<" ";
+    }
+   
+
+ 
+    return 0;
+}
+
